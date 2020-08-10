fe176f5e7544b85756f37961e5df1b3439446fe2
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-08-10 12:22:33 +0200

    cleaning files

--- src/login.tsx
+++ src/login.tsx
@@ -8,52 +8,23 @@ import TextField from "@material-ui/core/TextField";
 import { Typography } from "@material-ui/core";
 import Container from "@material-ui/core/Container";
 
-// Material UI ~ styles
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 500,
-    height: "100vh",
-    paddingTop: "100px",
-    textAlign: "center",
-    "& > *": {
-      margin: theme.spacing(1),
-    },
-  },
-  label: {
-    fontSize: "18px",
-    display: "inline-block",
-    marginRight: "10px",
-  },
-  flexContainer: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    padding: "10px",
-  },
-}));
-
 export const LoginPage: React.FC = () => {
   const history = useHistory();
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const [username, setUsername] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
   const classes = useStyles();
 
   const handleNavigation = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (username === "admin" && password === "test") {
-      history.push("/list");
-    } else {
-      alert("User / password not valid, psst... admin / test");
-    }
+    username === "admin" && password === "test"
+      ? history.push("/list")
+      : alert("User / password not valid, psst... admin / test");
   };
 
   return (
     <Container maxWidth="sm" className={classes.root}>
       <form onSubmit={handleNavigation}>
-        <Typography
-          variant="h2"
-          style={{ fontSize: "40px", marginBottom: "20px" }}
-        >
+        <Typography variant="h2" className={classes.h2}>
           User login
         </Typography>
         <div>
@@ -79,19 +50,43 @@ export const LoginPage: React.FC = () => {
             />
           </div>
         </div>
-
-        <Button
-          type="submit"
-          variant="contained"
-          style={{
-            backgroundColor: "#2BB686",
-            color: "white",
-            marginTop: "20px",
-          }}
-        >
+        <Button type="submit" variant="contained" className={classes.btn}>
           login
         </Button>
       </form>
     </Container>
   );
 };
+
+// Material UI ~ styles
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 500,
+    height: "100vh",
+    paddingTop: "100px",
+    textAlign: "center",
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  h2: {
+    fontSize: "40px",
+    marginBottom: "20px",
+  },
+  label: {
+    fontSize: "18px",
+    display: "inline-block",
+    marginRight: "10px",
+  },
+  flexContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "10px",
+  },
+  btn: {
+    backgroundColor: "#2BB686",
+    color: "white",
+    marginTop: "20px",
+  },
+}));
